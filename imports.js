ae24b6d3e25c923aa3ad576f079e58966e19de25
Juny <Juny-Real@users.noreply.github.com>
2017-12-07 20:41:26 -0500

    Create imports.js

---
+++ imports.js
@@ -0,0 +1,90 @@
+String.prototype.embedThis = function() {
+	var emb = {};
+	var t = "";
+	var d = "";
+	var c = 0;
+	var fields = new Array();
+	var footer = {};
+	var thumb = {};
+
+	if(Contains(this, "-t(")) {
+		t = this.split("-t(")[1];
+		if(Contains(t, "),")) {
+			t = t.split("),")[0];
+		}
+	}
+	if(Contains(this, "-d(")) {
+		d = this.split("-d(")[1];
+		if(Contains(d, "),")) {
+			d = d.split("),")[0];
+		}
+	}
+	if(Contains(this, "-c(")) {
+		c = this.split("-c(")[1];
+		if(Contains(c, "),")) {
+			c = c.split("),")[0];
+		}
+	}
+	if(Contains(this, "-f(")) {
+		ls = this.split("-f(");
+		for(i = 1; i < ls.length; i++) {
+			var dat = ls[i];
+			if(Contains(dat, "),")) {
+				var e = dat.split("),")[0];
+				if(Contains(e, "|")) {
+					var field = {};
+					// Name|Value|True
+					var data = e.split("|");
+					if(data.length == 3) {
+						field.name = e.split("|")[0];
+						field.value = e.split("|")[1];
+						if(e.split("|")[2].toLowerCase() == "true") {
+							field.inline = true;
+						} else {
+							field.inline = false;
+						}
+						fields.push(field);
+					}
+				}
+			}
+		}
+	}
+	if(Contains(this, "-ft(")) {
+		var f = this.split("-ft(")[1];
+		f = f.split("),")[0];
+		footer.text = f;
+	}
+	if(Contains(this, "-fi")) {
+		var f = this.split("-fi(")[1];
+		f = f.split("),")[0];
+		footer.icon_url = f;
+	}
+	if(Contains(this, "-tn")) {
+		var f = this.split("-tn(")[1];
+		f = f.split("),")[0];
+		thumb.url = f;
+	}
+	if(Contains(this, "-td")) {
+		var f = this.split("-td(")[1];
+		f = f.split("),")[0];
+		if(Contains(f, "x")) {
+			var ff = f.split("x");
+			thumb.height = ff[1];
+			thumb.width = ff[0];
+		}
+	}
+
+	emb.title = t;
+	emb.description = d;
+	emb.color = HTML2Int(c);
+	if(thumb != null) {
+		emb.thumbnail = thumb;
+	}
+	if(footer != null) {
+		emb.footer = footer;
+	}
+	if(fields.length > 0) {
+		emb.fields = fields;
+	}
+	return emb;
+}
