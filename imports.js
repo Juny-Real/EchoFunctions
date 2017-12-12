bf3f4fc8c21d5d7ea3b4e3c0ababbda83f902e1b
Juny <Juny-Real@users.noreply.github.com>
2017-12-11 21:36:27 -0500

    Update imports.js

--- imports.js
+++ imports.js
@@ -13,6 +13,67 @@ if (!String.prototype.includes) {
   };
 }
 
+if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function(callback/*, thisArg*/) {
+
+    var T, k;
+
+    if (this == null) {
+        throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling toObject() passing the
+    // |this| value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get() internal
+    // method of O with the argument "length".
+    // 3. Let len be toUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If isCallable(callback) is false, throw a TypeError exception. 
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let
+    // T be undefined.
+    if (arguments.length > 1) {
+        T = arguments[1];
+    }
+
+    // 6. Let k be 0.
+    k = 0;
+
+    // 7. Repeat while k < len.
+    while (k < len) {
+
+        var kValue;
+
+        // a. Let Pk be ToString(k).
+        //    This is implicit for LHS operands of the in operator.
+        // b. Let kPresent be the result of calling the HasProperty
+        //    internal method of O with argument Pk.
+        //    This step can be combined with c.
+        // c. If kPresent is true, then
+        if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal
+        // method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Call the Call internal method of callback with T as
+        // the this value and argument list containing kValue, k, and O.
+        callback.call(T, kValue, k, O);
+        }
+        // d. Increase k by 1.
+        k++;
+    }
+    // 8. return undefined.
+    };
+}
+
 
 
 String.prototype.embedThis = function() {
@@ -280,5 +341,117 @@ function unBitwise(data, type) {
             error.reason = "No Type Declaired"
             return "Error : "+error.reason;
         }
+    }
+}
+
+
+function hasPerm(userId, permission) {
+    var error = {"status":false};
+    var i;
+    var j;
+
+    //Check for errors in the input
+    if (!userID) {
+        error.status = true;
+        error.reason = "Missing userID";
+    } else if (!permission) {
+        error.status = true;
+        error.reason = "Missing permission";
+    } else if (typeof userId !== 'string') {
+        error.status = true;
+        error.reason = "Invalid userID (Not string)";
+    } else if (!Array.isArray(permission)) {
+        error.status = true;
+        error.reason = "Invalid permissions (Must be an array)";        
+    }
+
+
+    //end to deal with errors
+    if (error.status) {
+        return error.reason;
+    }
+
+
+    //validate the permissions
+    var matchedPerms = [];
+    var names = ["CREATE_INSTANT_INVITE","KICK_MEMBERS","BAN_MEMBERS","ADMINISTRATOR","MANAGE_CHANNELS","MANAGE_GUILD","ADD_REACTIONS","VIEW_AUDIT_LOG","VIEW_CHANNEL","SEND_MESSAGES","SEND_TTS_MESSAGES","MANAGE_MESSAGES","EMBED_LINKS","ATTACH_FILES","READ_MESSAGE_HISTORY","MENTION_EVERYONE","USE_EXTERNAL_EMOJIS","CONNECT","SPEAK","MUTE_MEMBERS","DEAFEN_MEMBERS","MOVE_MEMBERS","USE_VAD","CHANGE_NICKNAME","MANAGE_NICKNAMES","MANAGE_ROLES","MANAGE_WEBHOOKS","MANAGE_EMOJIS"];
+    var numbers = [0x00000001,0x00000002,0x00000004,0x00000008,0x00000010,0x00000020,0x00000040,0x00000080,0x00000400,0x00000800,0x00001000,0x00002000,0x00004000,0x00008000,0x00010000,0x00020000,0x00040000,0x00100000,0x00200000,0x00400000,0x00800000,0x01000000,0x02000000,0x04000000,0x08000000,0x10000000,0x20000000,0x40000000];
+    var numbname = ["0x00000001","0x00000002","0x00000004","0x00000008","0x00000010","0x00000020","0x00000040","0x00000080","0x00000400","0x00000800","0x00001000","0x00002000","0x00004000","0x00008000","0x00010000","0x00020000","0x00040000","0x00100000","0x00200000","0x00400000","0x00800000","0x01000000","0x02000000","0x04000000","0x08000000","0x10000000","0x20000000","0x40000000"];
+    permission.forEach(p => {
+        var i;
+        for (i=0;i < names.length;i++) {
+            if (p.toString().toUpperCase() === names[i]||parseInt(p) === numbers[i]||p.toLowerCase() === numbname[i]) {
+                matchedPerms.push(numbers[i]);
+            }
+        }
+    })
+    if (matchedPerms.length == 0) {
+        error.status = true;
+        error.reason = "Invalid Permissions (None found)";
+    }
+
+    //check for user and grab roles
+    var foundUser = false;
+    var arr = [];
+    for (i = 0;i < ServerMembers.length;i++) {
+        if (userId === ServerMembers[i].User.ID) {
+            foundUser = true
+            if (ServerMembers[i].Roles.length > 0) {
+                for (j = 0;j < ServerMembers[i].Roles.length;j++) {
+                    arr.push(ServerMembers[i].Roles[j]);
+                }
+            }
+        }
+    }
+    if (foundUser) {
+        error.status = true;
+        error.reason = "User not found"
+    }
+
+
+    //end to deal with errors
+    if (error.status) {
+        return error.reason;
+    }
+
+
+    //find role permissions
+    var bitfields = [];
+    if (arr.length > 0) {
+        for (i=0;i < arr.length;i++) {
+            for (j=0;j < ServerRoles.length;j++) {
+                if (arr[i] === ServerRoles[j].ID) {
+                    bitfields.push(ServerRoles[j].Permissions)
+                }
+            }
+        }
+    } else {
+        error.status = true;
+        error.reason = "User has no roles (No permissions collected)";
+    }
+
+
+    //check role permissions for given permissions
+    //bitfields and matchedPerms
+    var matches = 0;
+    for (i=0;i < matchedPerms.length;i++) {
+        for (j=0;j < bitfields.length;j++) {
+            if (matchedPerms[i] <= bitfields[j]) {
+                matches++;
+            }
+        }
+    }
+    if (matches !== matchedPerms.length) {
+        return "False";
+    } else if (matches === matchedPerms.length) {
+        return "True";
+    } else {
+        error.status = true;
+        error.reason = "Internal error (IDFK)";
+    }
+
+    //end to deal with errors
+    if (error.status) {
+        return error.reason;
     }
 }
