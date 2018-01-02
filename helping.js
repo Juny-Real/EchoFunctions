8bfd76ecc5ded0b1f9c9c5244b79ab83e2c967cb
Juny <Juny-Real@users.noreply.github.com>
2018-01-01 19:26:21 -0500

    Update helping.js

--- helping.js
+++ helping.js
@@ -1,3 +1,14 @@
+if (!Object.entries)
+  Object.entries = function( obj ){
+    var ownProps = Object.keys( obj ),
+        i = ownProps.length,
+        resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    
+    return resArray;
+  };
+
 function viewShop(user, shop) {
     if (!user) {
         throw new ReferenceError("Missing userObj! (Shop Command)");
