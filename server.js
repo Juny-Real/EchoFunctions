e34c6c0fc7b0aef88f098371874cacc1447a7b14
Juny <Juny-Real@users.noreply.github.com>
2018-02-16 12:25:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,24 +19,6 @@ for (i = 0; i < Server.Members.length; i++) {
 /*==*/info.chan = ServerChannels.length.toString();
 /*==*/info.role = ServerRoles.length.toString();
 
-var users = Server.Presences
-var online = 0;
-var botonline = 0;
-for (i=0;i < users.length;i++) {
-    if (users[i].Status !== "offline" && users[i].Status !== "") {
-        for (j=0;j < ServerMembers.length;j++) {
-			if (ServerMembers[j].User.ID === users[i].User.ID) {
-				if (ServerMembers[j].User.Bot === true) {
-					botonline++;
-				} else {
-					online++;
-				}
-			}
-		}
-    }
-}
-/*==*/info.onlinehumans = online.toString();
-/*==*/info.onlinebots = botonline.toString();
 
 function System() {
     this.getUnicodePackage = function(pkg) {
@@ -122,24 +104,73 @@ if (check4 >= 2) {
 	throw new ReferenceError("Invalid server object!");
 }
 
-var iconUrl = 'https://cdn.discordapp.com/icons/'+Server.ID+'/'+Server.Icon+'.jpg';
-var colorHex = ["#ff0000","#00ff00","#ffffff","#4286f4","#f45642","#262525","#e2d626","#87e226","#26e2c0","#2633e2","#8126e2"];
-var random = Math.floor(Math.random() * (colorHex.length - 0 + 1) ) + 0;
-var emb = {};
-emb.author = {"name":Server.Name,"url":"https://discord.gg/dbAgErg","icon_url":iconUrl};
-emb.thumbnail = {"url":iconUrl,"height":250,"width":250};
-emb.fields = [
-    {"name":"Server Name","value":Server.Name,"inline":true},
-    {"name":"Server ID","value":Server.ID,"inline":true},
-    {"name":"Server Owner","value":info.owner,"inline":true},
-    {"name":"Server Region","value":Server.Region,"inline":true},
-    {"name":"Server Humans","value":info.users,"inline":true},
-    {"name":"Server Bots","value":info.bots,"inline":true},
-    {"name":"Humans Online","value":info.onlinehumans,"inline":true},
-    {"name":"Bots Online","value":info.onlinebots,"inline":true},
-    {"name":"Server Channels","value":info.chan,"inline":true},
-    {"name":"Server Roles","value":info.role,"inline":true}
-];
-emb.color = HTML2Int(colorHex[random]);
-emb.footer = {"text":"Milestones : "+milestone}
-resp = emb;
+
+
+if (ServerMembers.length >= 100) {
+	var iconUrl = 'https://cdn.discordapp.com/icons/'+Server.ID+'/'+Server.Icon+'.jpg';
+	var colorHex = ["#ff0000","#00ff00","#ffffff","#4286f4","#f45642","#262525","#e2d626","#87e226","#26e2c0","#2633e2","#8126e2"];
+	var random = Math.floor(Math.random() * (colorHex.length - 0 + 1) ) + 0;
+	var emb = {};
+	emb.author = {"name":Server.Name,"url":"https://discord.gg/dbAgErg","icon_url":iconUrl};
+	emb.thumbnail = {"url":iconUrl,"height":250,"width":250};
+	emb.fields = [
+		{"name":"Server Name","value":Server.Name,"inline":true},
+		{"name":"Server ID","value":Server.ID,"inline":true},
+		{"name":"Server Owner","value":info.owner,"inline":true},
+		{"name":"Server Region","value":Server.Region,"inline":true},
+		{"name":"Server Humans","value":info.users,"inline":true},
+		{"name":"Server Bots","value":info.bots,"inline":true},
+		{"name":"Server Channels","value":info.chan,"inline":true},
+		{"name":"Server Roles","value":info.role,"inline":true}
+	];
+	var time = new Date();
+	emb.timestamp = time.toISOString();
+	emb.color = HTML2Int(colorHex[random]);
+	emb.footer = {"text":milestone}
+	resp = emb;
+} else {
+	var users = Server.Presences
+	var online = 0;
+	var botonline = 0;
+	for (i=0;i < users.length;i++) {
+		if (users[i].Status !== "offline" && users[i].Status !== "") {
+			for (j=0;j < ServerMembers.length;j++) {
+				if (ServerMembers[j].User.ID === users[i].User.ID) {
+					if (ServerMembers[j].User.Bot === true) {
+						botonline++;
+					} else {
+						online++;
+					}
+				}
+			}
+		}
+	}
+
+	/*==*/info.onlinehumans = online.toString();
+	/*==*/info.onlinebots = botonline.toString();
+
+
+	var iconUrl = 'https://cdn.discordapp.com/icons/'+Server.ID+'/'+Server.Icon+'.jpg';
+	var colorHex = ["#ff0000","#00ff00","#ffffff","#4286f4","#f45642","#262525","#e2d626","#87e226","#26e2c0","#2633e2","#8126e2"];
+	var random = Math.floor(Math.random() * (colorHex.length - 0 + 1) ) + 0;
+	var emb = {};
+	emb.author = {"name":Server.Name,"url":"https://discord.gg/dbAgErg","icon_url":iconUrl};
+	emb.thumbnail = {"url":iconUrl,"height":250,"width":250};
+	emb.fields = [
+		{"name":"Server Name","value":Server.Name,"inline":true},
+		{"name":"Server ID","value":Server.ID,"inline":true},
+		{"name":"Server Owner","value":info.owner,"inline":true},
+		{"name":"Server Region","value":Server.Region,"inline":true},
+		{"name":"Server Humans","value":info.users,"inline":true},
+		{"name":"Server Bots","value":info.bots,"inline":true},
+		{"name":"Humans Online","value":info.onlinehumans,"inline":true},
+		{"name":"Bots Online","value":info.onlinebots,"inline":true},
+		{"name":"Server Channels","value":info.chan,"inline":true},
+		{"name":"Server Roles","value":info.role,"inline":true}
+	];
+	var time = new Date();
+	emb.timestamp = time.toISOString();
+	emb.color = HTML2Int(colorHex[random]);
+	emb.footer = {"text":milestone}
+	resp = emb;
+}
